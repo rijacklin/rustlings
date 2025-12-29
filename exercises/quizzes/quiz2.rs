2afc22a89880090132000c33e9235bece2320581
rijacklin <rijacklin1@gmail.com>
2025-12-29 13:11:42 -0500

    Finished quiz 1 and section 4

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: (String, Command)) -> String {
+        let (s, cmd) = input;
+
+        let result = match cmd {
+            Command::Uppercase => s.to_uppercase(),
+            Command::Trim => s.trim().to_string(),
+            Command::Append(n) => String::from("bar").repeat(n),
+        };
+
+        result
+    }
 }
 
 fn main() {
@@ -37,8 +47,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
